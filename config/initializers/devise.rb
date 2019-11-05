b09a0b5906c6fd99335a1e9732897004dd8f27ac
Dom Tunstill <datunstill@gmail.com>
2019-11-05 17:39:35 +0000

    add secret for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,9 +8,11 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '34776925652d3f0ecd5129595ae6e296c6764edb5c3b885e100a1384e30dfc36c95b3c11cd8850a9120125caa1bdbbdf441d3929578216870121be88dda9afd4'
+  # rails generate devise:install
 
-  # ==> Controller configuration
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
+
+  #===> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
